5e99e94c254ce264d0e8c538700581819ced42df
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2022-12-10 17:35:50 +0100

    HECH20
    Register Page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,15 @@
+import "./register.css";
+
 export default function Register() {
     return (
-        <div>
-        <h1>Register</h1>
-        <p></p>
+        <div className="register-window">
+            <h1>Register</h1>
+            <p>Already registered, <a href="/login">sign in</a></p>
+            <input type="text" placeholder="Username"/>
+            <input type="email" placeholder="E-Mail"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Password"/>
+            <button>Register</button>
         </div>
     );
 }
