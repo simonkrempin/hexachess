85753241b9f4ddb94db2cd03d5bd283b28cb741b
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2023-05-13 11:03:00 +0200

    rebase

---
+++ src/models/boardStateModel.ts
@@ -0,0 +1,14 @@
+interface BoardStateModel {
+    bPawns: string;
+    bRooks: string;
+    bKnights: string;
+    bBishops: string;
+    bQueen: string;
+    bKing: string;
+    wPawns: string;
+    wRooks: string;
+    wKnights: string;
+    wBishops: string;
+    wQueen: string;
+    wKing: string;
+}
